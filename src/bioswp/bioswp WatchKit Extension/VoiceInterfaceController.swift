8e5cf2f350fe9a9655a83169ee893e00e17cee8f
sthordall <stephantl@gmail.com>
2016-06-21 20:59:10 +0200

    Voice recording watch-side implemented

--- src/bioswp/bioswp WatchKit Extension/VoiceInterfaceController.swift
+++ src/bioswp/bioswp WatchKit Extension/VoiceInterfaceController.swift
@@ -30,6 +30,50 @@ class VoiceInterfaceController : WKInterfaceController, WCSessionDelegate {
         super.awakeWithContext(context)
         localContext = context as? VoiceInterfaceContext
         instructionLabel.setText(localContext?.instruction)
-        
+        if let location = localContext?.dataStorePath {
+            rec(location)
+        }
+    }
+    
+    func rec(toFile: String) {
+        guard let duration = localContext?.sampleDuration else { return }
+        if let recFileUrl = recFileURL(toFile) {
+            self.presentAudioRecorderControllerWithOutputURL(
+                recFileUrl,
+                preset: WKAudioRecorderPreset.WideBandSpeech,
+                options: [WKAudioRecorderControllerOptionsMaximumDurationKey: NSTimeInterval(duration)],
+                completion: { (didSave, error) -> Void in
+                    print("error: \(error)\n")
+                    if didSave {
+                        self.storeVoiceData(toFile, dataUrl: recFileUrl)
+                    } else { self.popController() }
+            })
+        } else { self.popController() }
+    }
+    
+    func recFileURL(fileName:String) -> NSURL? {
+        if let container = NSFileManager.defaultManager().containerURLForSecurityApplicationGroupIdentifier("group.io.sthordall.app.bioswp") {
+            return container.URLByAppendingPathComponent(fileName, isDirectory: false)
+        } else {
+            return nil
+        }
+    }
+    
+    func storeVoiceData (location: String, dataUrl: NSURL) {
+        if WCSession.isSupported() {
+            self.session = WCSession.defaultSession()
+            let message = ["location": location, "dataUrl": dataUrl]
+            self.session!.sendMessage(message,
+                                      replyHandler: {(response) -> Void in
+                                        print("Succesful voice msg to phone")
+                                        self.localContext?.completionClosure()
+                                        self.popController()
+                                      },
+                                      errorHandler: {(error) -> Void in
+                                        print("Un-succesful voice msg to phone")
+                                        self.localContext?.completionClosure()
+                                        self.popController()
+                                      })
+        }
     }
 }
